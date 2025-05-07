4e990f802f1e73033663a1877a89acf6b23b9bdb
Tianhao-Gu <tgu@anl.gov>
2025-05-06 19:55:50 -0500

    better way to start app

---
+++ scripts/mcp-server-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# First run the original setup script defined in the cdm-spark-standalone image
+# https://github.com/kbase/cdm-spark-standalone/blob/main/scripts/entrypoint.sh#L3
+. /opt/scripts/setup.sh
+
+# skip launching Spark - we use this container as spark driver
+
+exec python -m src.main
